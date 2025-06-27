beba30a430c877a236f8b5e9d4bbd75c59125b0b
Mutebi Abdul Hakeem <124655544+Keemcodes1@users.noreply.github.com>
2025-06-27 16:41:38 +0300

    Update settings.py

--- afriWeb/settings.py
+++ afriWeb/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'afriweb.urls'
+ROOT_URLCONF = 'afriWeb.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'afriweb.wsgi.application'
+WSGI_APPLICATION = 'afriWeb.wsgi.application'
 
 
 # Database
