649e0bb6bb665f573a69f8ac0601c8fd0c9fea7a
Mutebi Abdul Hakeem <124655544+Keemcodes1@users.noreply.github.com>
2025-06-30 14:51:04 +0300

    Update wsgi.py

--- afriWeb/wsgi.py
+++ afriWeb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'afriWeb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'afriweb.settings')
 
 application = get_wsgi_application()
